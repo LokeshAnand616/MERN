9c85d50327ee4bc52310ff3bbb77b813dcaaaa64
LokeshAnand616 <lokesh.anand@aspiresys.com>
2025-01-07 19:01:59 +0530

    7th january

---
+++ JavaScript/module1.js
@@ -0,0 +1,13 @@
+// module1.js
+// Exporting a function and an object from module1.js
+
+// Function export
+export function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+// Object export
+export const person = {
+    name: 'Alice',
+    age: 25
+};
