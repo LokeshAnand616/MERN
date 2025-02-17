be79ec76987a5e4e02c3dbb714f2250f130720c7
Asplap1006 <lokesh.anand@aspiresys.com>
2025-02-17 18:21:11 +0530

    added files

--- redux/blog-post/src/features/users/userSlice.js
+++ redux/blog-post/src/features/users/userSlice.js
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-    { id: 0, user: "lokesh" },
-    { id: 1, user: "kannan" },
-    { id: 2, user: "bala" },
-    { id: 3, user: "rathesh" },
+  { id: 0, user: "lokesh" },
+  { id: 1, user: "kannan" },
+  { id: 2, user: "bala" },
+  { id: 3, user: "rathesh" },
 ];
 
 const userSlice = createSlice({
-    name: "users",
-    initialState,
-    reducers: {},
+  name: "users",
+  initialState,
+  reducers: {},
 });
 
 export default userSlice.reducer;
-export const selectAllUsers = (state) => state.users;  
+export const selectAllUsers = (state) => state.users;
